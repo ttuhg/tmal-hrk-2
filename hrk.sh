8e96571527726fcf14ee938a6090158d4b5a19bd
ttuhg <63356932+ttuhg@users.noreply.github.com>
2022-12-28 17:30:14 +0800

    Update hrk.sh

--- hrk.sh
+++ hrk.sh
@@ -18,4 +18,4 @@ cp /root/tmal-hrk-2/index.html /workspace/index.html
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo "Asia/Shanghai" > /etc/timezone
 sed -i "s/REBOOTDATE/$(date)/g" /workspace/index.html
-/usr/bin/supervisord -c /etc/supervisord.conf &
+/usr/bin/supervisord -c /etc/supervisord.conf
